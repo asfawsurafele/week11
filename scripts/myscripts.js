9117d73f8752b45f6184f27603aa0e067cf36868
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-15 16:23:29 -0800

    Functions practice

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -29,15 +29,72 @@
 // greet('Surafele')
 // greet('Surafele', 'Night',)
 // // greet(`Surafele`);
-let r = prompt('Enter a radius:');
-console.log(r)
+// let r = prompt('Enter a radius:');
+// console.log(r)
 
-function circleArea (radius)
+// function circleArea (radius)
+// {
+//     return Math.PI * radius * radius;
+// }
+
+
+// let area = circleArea(r);
+
+// alert(`The area: ${area.toFixed(2)}`);
+
+// let r = prompt('Enter a radius:');
+// console.log(r)
+
+// function circleArea (radius)
+// {
+//     let radiusP = document.querySelector('#radius');
+//     let resultP = document.querySelector('#result');
+//         radiusP.textContent = radius;
+//     let final = Math.PI * radius * radius;
+//     resultP.textContent = final;
+//     return resultP;
+// }
+
+// const answer = circleArea(r)
+
+// alert(`The area of a circle with radius ${r} is ${answer}`)
+const unorderedList = document.querySelector(".shopping");
+
+function populateList(list)
 {
-    return Math.PI * radius * radius;
+    list.forEach(element => {
+    let listItem = document.createElement("li");
+    listItem.textContent = element
+    unorderedList.appendChild(listItem);
+    
+    });
 }
 
+const shoppingList =['Cheese', 'bread', 'green pepper']
+populateList(shoppingList)
 
-let area = circleArea(r);
+function switchclass (){
+unorderedList.classList.remove('circlelist');
+unorderedList.classList.add('squareList');
+}
+switchclass()
 
-alert(`The area: ${area.toFixed(2)}`);
+const liItems = document.querySelectorAll('.shopping li')
+
+// liItems.forEach(element => {
+//     if (element != 'green'){
+//         element = 'green'
+//     }
+// });
+
+function changeColor(list){
+    list.forEach(element => {
+        if (element.textContent.includes('green')){
+            element.classList.add('green')
+        };
+    }
+)};
+
+changeColor(liItems)
+
+
